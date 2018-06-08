1c1ea4a4e920471085239e6aaba8e6515f15bede
socs <socs@socsvm>
2018-06-07 20:14:05 -0400

    Updated

--- cis2520-lab02/src/HashTableAPI.c
+++ cis2520-lab02/src/HashTableAPI.c
@@ -10,8 +10,8 @@ HTable *createTable(size_t size, int (*hashFunction)(size_t tableSize, int key),
     newHTable->hashFunction = hashFunction;
     newHTable->printData = printData;
     newHTable->table = malloc(sizeof(Node*)*size);
-	
-    for (int i = 0; i < size; ++i) 
+
+    for (int i = 0; i < size; ++i)
     {
 		newHTable->table[i] = malloc(sizeof(Node));
 		newHTable->table[i] = NULL;
@@ -26,17 +26,17 @@ Node *createNode(int key, void *data)
     newNode->next = NULL;
     newNode->key = key;
     newNode->data = data;
-    
+
     return newNode;
 }
 void destroyTable(HTable *hashTable)
 {
     Node *tempNode;
-	for (int i = 0; i < hashTable->size ; i++) 
+	for (int i = 0; i < hashTable->size ; i++)
     {
 		tempNode = hashTable->table[i];
-		while (tempNode != NULL) 
-        {
+		while (tempNode != NULL)
+      {
 			removeData(hashTable, i, temp->data);
 			tempNode = tempNode->next;
 		}
@@ -44,12 +44,25 @@ void destroyTable(HTable *hashTable)
 
     free(hashTable->table);
 	free(hashTable);
-} 
+}
 void insertData(HTable *hashTable, int key, void *data)
 {
     if(HTable != NULL)
     {
-        index = 
+        Node *newNode = createNode(key, data);
+        int index = hashTable->hashFunction(hashTable->size, key);
+
+        if(hashTable->table[index] != NULL)
+        {
+          Node *temp = hashTable->table[key];
+		      hashTable->table[key] = newNode;
+		      hashTable->table[key]->next = temp;
+        }
+
+        else
+        {
+          hashTable->table[key] = newNode;
+        }
     }
 if (hashTable->table[key] == NULL) {
 
@@ -63,5 +76,5 @@ void removeData(HTable *hashTable, int key)
 }
 void *lookupData(HTable *hashTable, int key)
 {
-
+ int loc = hashTable->hashFunction(hashTable->size, key);
 }
